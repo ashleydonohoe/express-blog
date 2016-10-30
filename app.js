7acbb6700054ff74390969a88e2f00b15c225299
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-10-30 02:23:30 +0000

    added show page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// App Config
 var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
@@ -8,6 +9,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Mongoose/Model Config
 var blogSchema = new mongoose.Schema({
    title: String,
    image: String,
@@ -15,11 +17,54 @@ var blogSchema = new mongoose.Schema({
    created: {type: Date, default: Date.now}
 });
 
-// Mongoose/Model Config
 var Blog = mongoose.model("Blog", blogSchema);
 
+// Blog.create({
+//     title: "Test Blog",
+//     image: "https://images.unsplash.com/photo-1464983953574-0892a716854b?ixlib=rb-0.3.5&q=80&fm=jpg",
+//     body: "This is a test post"
+// })
+
+// Routes
+app.get("/blogs", function(req, res) {
+    Blog.find({}, function(err, blogs) {
+       if(err) {
+           console.log("Error!");
+       } else {
+           res.render("index", {blogs: blogs});
+       }
+    });
+});
+
+app.get("/blogs/new", function(req, res) {
+   res.render("new"); 
+});
+
+app.post("/blogs", function(req, res) {
+   // create
+   Blog.create(req.body.blog, function(err, newBlog) {
+       if(err) {
+           res.render("new");
+       } else {
+           res.redirect("/blogs");
+       }
+   });
+});
+
+// Show Route
+app.get("/blogs/:id", function(req, res) {
+   Blog.findById(req.params.id, function(err, foundBlog) {
+      if(err) {
+          res.redirect("/blogs");
+      } else {
+          res.render("show", {blog: foundBlog});
+      }
+   });
+});
+
+
 app.get("/", function(req, res) {
-   res.send("Testing"); 
+    res.redirect("/blogs");
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
